2e4518c3ba5e462c76aa9f6aad4cc8fe29550dbb
Sergii Rykov <serenya@live.com>
2025-10-16 16:30:04 +0200

    Final version of train BPE

--- src/train_bpe.py
+++ src/train_bpe.py
@@ -1,6 +1,6 @@
 import os
 import time
-from multiprocessing.pool import Pool
+from collections import Counter
 import concurrent.futures
 import regex as re
 from cs336_basics.pretokenization_example import find_chunk_boundaries
@@ -12,10 +12,16 @@ def train_bpe(
     special_tokens: list[str]
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
 
+    vocabulary = build_initial_vocabulary(special_tokens)
+    pretokenized_dict = pretokenize(input_path, special_tokens)
+    paired_tokens = build_token_pairs(pretokenized_dict)
+    merges = compute_merges(vocabulary, vocab_size, pretokenized_dict, paired_tokens)
+    return (vocabulary, merges)
+
+def build_initial_vocabulary(
+    special_tokens: list[str]
+) -> dict[int, bytes]:
     vocabulary = {}
-    merges = []
-    chunks_of_pretokenized_dict = []
-    paired_tokens = {}
     for i in range(len(special_tokens)):
         vocabulary[i] = special_tokens[i].encode("utf-8")
 
@@ -24,195 +30,78 @@ def train_bpe(
     for index in all_bytes:
         vocabulary[special_tokens_shift + index] = bytes([index])
 
+    return vocabulary
+
+def pretokenize(
+    input_path: str,
+    special_tokens: list[str]
+) -> dict[tuple[bytes], int]:
     start_time = time.time()
-    ## Usage
-    """ boundaries = []
-    with open(input_path, "rb") as f:
-        num_chunks = 500
-        boundaries = find_chunk_boundaries(f, num_chunks, b"<|endoftext|>") """
-
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-
-    #multiple_results = []
-    #process_id = 1
+    pretokenized_counter = Counter()
     try:
-        num_processes = 4
+        num_processes = 16
+        futures = []
         with concurrent.futures.ProcessPoolExecutor(num_processes) as executor:
-            futures = [executor.submit(read_file_in_chunks, str) for str in read_chunks(input_path)]
+            with open(input_path, "rb") as f: # Move to the end of the file to ensure it's accessible
+                boundaries = find_chunk_boundaries(f, 4 * num_processes, b"<|endoftext|>")
+            
+            special_tokens_PAT = re.compile("|".join([re.escape(st) for st in special_tokens]))
+            PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+            regex_instance = re.compile(PAT)
+            futures = [executor.submit(read_file_in_chunks, input_path, start, end, special_tokens_PAT, regex_instance) for start, end in zip(boundaries[:-1], boundaries[1:])]
             for future in concurrent.futures.as_completed(futures):
-                res = future.result()
-                print(f"Got result of length {len(res)}.")
-                chunks_of_pretokenized_dict += res
-        """ with Pool(processes=num_processes) as pool:
-            #chunks = []
-            for result in pool.map(read_file_in_chunks, read_chunks(input_path), chunksize=1):
-                # result is ready as soon as processed
-                print(f"Got result of length {len(result)}.")
-                #chunks_of_pretokenized_dict += result """
+                pretokenized_counter += future.result()
+            
+            print(f"Length of words counter: {len(pretokenized_counter)}")
+           
         print(f"Finished processing all chunks with {num_processes} processes.")
     except Exception as e:
         print(f"Main process error: {e}")
-    """ for start, end in zip(boundaries[:-1], boundaries[1:]):
-        chunks_of_pretokenized_dict = []
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        print(f"Reading chunk from {start} to {end}...") """
-
-
-    #chunks.append(chunk)
-            #print(f"Scheduling chunk from {start} to {end} with process_id {process_id}...")
-            #items.append((f, start, end, special_tokens))
-            #items.append((input_path, process_id, start, end, special_tokens))
-
-    """ result = pool.starmap_async(read_file_in_chunks, [(chunk,special_tokens) for chunk in chunks], chunksize=16)
-    results = result.get()
-    for r in results:
-        chunks_of_pretokenized_dict += r """
-        
-    """ result = pool.apply_async(read_file_in_chunks, (chunk, process_id, special_tokens))#(input_path, process_id, start, end, special_tokens))
-    multiple_results.append(result)
-
-    process_id += 1 """
-
-    """ if len(multiple_results) >= 2 * num_processes:
-        for result in multiple_results:
-            #print("Getting result...")
-            get_result = result.get()
-            print(f"Got result of length {len(get_result)}.")
-            chunks_of_pretokenized_dict += get_result
-
-        multiple_results = []
-        process_id = 1 """
-
-    """ if len(multiple_results) >= 10:
-        process_id = 1
-
-        
-
-        multiple_results = []
-
-        print(f"Processed 10% chunks so far...") """
-    """ for result in multiple_results:
-        #print("Getting result...")
-        get_result = result.get()
-        print(f"Got result of length {len(get_result)}.")
-        chunks_of_pretokenized_dict += get_result """
-
-    #items = []
-    
-        
-
-
-
-    
-
-    """ multiple_results = []
-    for item in items:
-        result = pool.apply_async(read_file_in_chunks, item)
-        multiple_results.append(result)
-    
-    for result in multiple_results:
-        chunks_of_pretokenized_dict += result.get() """
 
     end_time = time.time()
+
     print(f"Time to read and pretokenize: {end_time - start_time} seconds")
 
-    """ start_time = time.time()
-
-    for pretokenized_dict in chunks_of_pretokenized_dict:
-        paired_tokens = build_token_pairs(pretokenized_dict, paired_tokens)
-
-    end_time = time.time()
-    print(f"Time to build token pairs: {end_time - start_time} seconds")
-
-    start_time = time.time()
-
-    for i in range(vocab_size - len(vocabulary)):
-        if len(paired_tokens) == 0:
-            break
-
-        most_frequent_pair = find_most_frequent_pair(paired_tokens)
-        token_id = len(vocabulary)
-        vocabulary[token_id] = most_frequent_pair[0] + most_frequent_pair[1]
-        merges.append(most_frequent_pair)
-
-        for merged_tokens in chunks_of_pretokenized_dict:
-            merge_tokens(merged_tokens, most_frequent_pair, paired_tokens)
-
-    end_time = time.time()
-    print(f"Time to build BPE merges: {end_time - start_time} seconds") """
-
-    return (vocabulary, merges)
-
-def read_chunks(input_path):
-    with open(input_path, "rb") as f:
-        num_chunks = 160
-        boundaries = find_chunk_boundaries(f, num_chunks, b"<|endoftext|>")
-        for start, end in zip(boundaries[:-1], boundaries[1:]):
-            if end > 176938507:
-                continue
-            f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            print(f"Reading chunk from {start} to {end}...")
-            yield chunk
+    return { tuple(map(int.to_bytes, key.encode("utf-8"))):value for key, value in pretokenized_counter.items() }
 
 def read_file_in_chunks(
-    chunk: str,
-    #f: BinaryIO,
-    #input_path: str | os.PathLike,
-    #process_id: int,
-    #start: int,
-    #end: int,
-    #special_tokens: list[str],
-) -> list[dict[tuple[bytes], int]]:
+    input_path: str | os.PathLike,
+    start: int,
+    end: int,
+    special_tokens_PAT: re.Pattern[str],
+    regex_instance: re.Pattern[str]
+) -> Counter[str]:
     try:
-        print(f"Scheduled chunk begin")
-        special_tokens = ["<|endoftext|>"]
-        i = 0
-        chunks_of_pretokenized_dict = []
-        """ with open(input_path, "rb") as f:
-            
+        counter = Counter()
+        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        with open(input_path, "rb") as f:
             f.seek(start)
             chunk = f.read(end - start).decode("utf-8", errors="ignore")
-    """        # Run pre-tokenization on your chunk and store the counts for each pre-token
-            
-        for content in re.split("|".join([re.escape(st) for st in special_tokens]), chunk):
-            if content.strip() == "":
-                continue
-            """ if i > 0 and i % 10000 == 0:
-                print(f"With process_id {process_id}, processed {i} chunks...") """
+            for content in special_tokens_PAT.split(chunk):
+                if content.strip() == "":
+                    continue
 
-            pretokenized_dict = build_pretokenized_dict(content)
-            chunks_of_pretokenized_dict.append(pretokenized_dict)
-            i += 1
-
-        print(f"Scheduled chunk with process_id finished with length {len(chunks_of_pretokenized_dict)}.")
-        return chunks_of_pretokenized_dict
+                counter = build_pretokenized_counter(content, counter, regex_instance)
+        return counter
     except Exception as e:
-        # You can log or return an error marker instead of crashing the worker
         return f"Custom Error: {e}"
 
-def build_pretokenized_dict(
-    content: str
-) -> dict[tuple[bytes], int]:
-    pretokenized_dict = {}
-    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+def build_pretokenized_counter(
+    content: str,
+    counter: Counter[str],
+    pattern: re.Pattern[str]
+) -> Counter[str]:
+    for match in pattern.finditer(content):
+        counter[match.group()] += 1
+
+    return counter
     
-    for match in re.finditer(PAT, content):
-        matched_tuple = tuple(map(int.to_bytes, match.group().encode("utf-8")))
-
-        existing_pretokenized_item_count = pretokenized_dict.get(matched_tuple, 0)
-
-        pretokenized_dict[matched_tuple] = existing_pretokenized_item_count + 1
-
-    return pretokenized_dict
-    
-
 def build_token_pairs(
-    pretokenized_dict: dict[tuple[bytes], int],
-    paired_tokens: dict[tuple[bytes, bytes], int]
+    pretokenized_dict: dict[tuple[bytes], int]
 ) -> dict[tuple[bytes, bytes], int]: 
+    start_time = time.time()
+
+    paired_tokens = {}
     for token_tuple, count in pretokenized_dict.items():
         if len(token_tuple) == 1:
             continue
@@ -221,6 +110,10 @@ def build_token_pairs(
             pair = (token_tuple[i], token_tuple[i + 1])
             existing_pair_count = paired_tokens.get(pair, 0)
             paired_tokens[pair] = existing_pair_count + count
+
+    end_time = time.time()
+
+    print(f"Time to build token pairs: {end_time - start_time} seconds")
 
     return paired_tokens
 
@@ -231,15 +124,41 @@ def find_most_frequent_pair(
 
     return max([k for k, v in paired_tokens.items() if v == max_entry_count]) # lexicographically greater pair if multiple max
 
+def compute_merges(
+    vocabulary: dict[int, bytes],
+    vocab_size: int,
+    pretokenized_dict: dict[tuple[bytes], int],
+    paired_tokens: tuple[bytes, bytes],
+) -> list[tuple[bytes, bytes]]:
+    start_time = time.time()
+
+    merges = []
+
+    for i in range(vocab_size - len(vocabulary)):
+        """ if len(paired_tokens) == 0:
+            return (vocabulary, merges)
+         """
+        most_frequent_pair = find_most_frequent_pair(paired_tokens)
+        token_id = len(vocabulary)
+        vocabulary[token_id] = most_frequent_pair[0] + most_frequent_pair[1]
+        merges.append(most_frequent_pair)
+        merge_tokens(pretokenized_dict, most_frequent_pair, paired_tokens)
+
+    end_time = time.time()
+
+    print(f"Time to build BPE merges: {end_time - start_time} seconds")
+
+    return merges
+
 def merge_tokens(
-    byte_pair_frequency: dict[tuple[bytes], int],
+    merged_tokens: dict[tuple[bytes], int],
     most_frequent_pair: tuple[bytes, bytes],
     paired_tokens: dict[tuple[bytes], int]
 ):
     new_merged_tokens = {}
     old_merged_tokens = []
 
-    for token_tuple, count in byte_pair_frequency.items():
+    for token_tuple, count in merged_tokens.items():
         any_merges = False
         skip_next_pair = False
         is_last_token_merged = False
@@ -273,8 +192,6 @@ def merge_tokens(
                     existing_pair_count = paired_tokens.get(pair, 0)
                     paired_tokens[pair] = existing_pair_count + count
 
-                    
-
                 if i + 2 < len(token_tuple):
                     right_pair = (token_tuple[i + 1], token_tuple[i + 2])
                     existing_right_pair_count = paired_tokens.get(right_pair, 0)
@@ -297,14 +214,7 @@ def merge_tokens(
             new_merged_tokens[tuple(token_tuple_values)] = count
 
     for old_token_tuple in old_merged_tokens:
-        del byte_pair_frequency[old_token_tuple]
+        del merged_tokens[old_token_tuple]
 
     for new_token_tuple, count in new_merged_tokens.items():
-        byte_pair_frequency[new_token_tuple] = count
-
-if __name__ == "__main__":
-    train_bpe(
-        input_path=FIXTURES_PATH / "data/TinyStoriesV2-GPT4-train.txt",
-        vocab_size=10000,
-        special_tokens=["<|endoftext|>"],
-    )
+        merged_tokens[new_token_tuple] = count
