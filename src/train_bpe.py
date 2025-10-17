7de22e19d16308f739b8bd4d2201724f9cd033c2
Sergii Rykov <serenya@live.com>
2025-10-17 13:22:13 +0200

    Add progress info

--- src/train_bpe.py
+++ src/train_bpe.py
@@ -14,7 +14,9 @@ def train_bpe(
 
     vocabulary = build_initial_vocabulary(special_tokens)
     pretokenized_dict = pretokenize(input_path, special_tokens)
+    
     paired_tokens = build_token_pairs(pretokenized_dict)
+
     merges = compute_merges(vocabulary, vocab_size, pretokenized_dict, paired_tokens)
     return (vocabulary, merges)
 
@@ -39,20 +41,25 @@ def pretokenize(
     start_time = time.time()
     pretokenized_counter = Counter()
     try:
-        num_processes = 16
+        num_processes = 8
         futures = []
         with concurrent.futures.ProcessPoolExecutor(num_processes) as executor:
             with open(input_path, "rb") as f: # Move to the end of the file to ensure it's accessible
-                boundaries = find_chunk_boundaries(f, 4 * num_processes, b"<|endoftext|>")
+                boundaries = find_chunk_boundaries(f, 40 * num_processes, b"<|endoftext|>")
             
-            special_tokens_PAT = re.compile("|".join([re.escape(st) for st in special_tokens]))
+            print(f"Length of chunks: {len(boundaries)}")
+            compiled_special_tokens_re = re.compile("|".join([re.escape(st) for st in special_tokens]))
             PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-            regex_instance = re.compile(PAT)
-            futures = [executor.submit(read_file_in_chunks, input_path, start, end, special_tokens_PAT, regex_instance) for start, end in zip(boundaries[:-1], boundaries[1:])]
+            compiled_words_re = re.compile(PAT)
+            futures = [executor.submit(read_file_in_chunks, input_path, start, end, compiled_special_tokens_re, compiled_words_re) for start, end in zip(boundaries[:-1], boundaries[1:])]
+            i = 1
             for future in concurrent.futures.as_completed(futures):
-                pretokenized_counter += future.result()
+                result = future.result()
+                pretokenized_counter.update(result)
+                print(f"\rRead and pretokenize iteration: {i}, Length of pretokenized words counter: {len(pretokenized_counter)}", end="", flush=True)
+                i += 1
             
-            print(f"Length of words counter: {len(pretokenized_counter)}")
+            print(f"\nLength of pretokenized words counter: {len(pretokenized_counter)}")
            
         print(f"Finished processing all chunks with {num_processes} processes.")
     except Exception as e:
@@ -68,20 +75,21 @@ def read_file_in_chunks(
     input_path: str | os.PathLike,
     start: int,
     end: int,
-    special_tokens_PAT: re.Pattern[str],
-    regex_instance: re.Pattern[str]
+    compiled_special_tokens_re: re.Pattern[str],
+    compiled_words_re: re.Pattern[str]
 ) -> Counter[str]:
     try:
         counter = Counter()
+
         # Run pre-tokenization on your chunk and store the counts for each pre-token
         with open(input_path, "rb") as f:
             f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            for content in special_tokens_PAT.split(chunk):
+            chunk = f.read(end - start).decode("utf-8", errors="replace")
+            for content in compiled_special_tokens_re.split(chunk):
                 if content.strip() == "":
                     continue
 
-                counter = build_pretokenized_counter(content, counter, regex_instance)
+                counter = build_pretokenized_counter(content, counter, compiled_words_re)
         return counter
     except Exception as e:
         return f"Custom Error: {e}"
@@ -117,27 +125,22 @@ def build_token_pairs(
 
     return paired_tokens
 
-def find_most_frequent_pair(
-    paired_tokens: dict[tuple[bytes, bytes], int],    
-) -> tuple[bytes, bytes]:
-    max_entry_count = max(paired_tokens.values()) # max count of any pair
-
-    return max([k for k, v in paired_tokens.items() if v == max_entry_count]) # lexicographically greater pair if multiple max
-
 def compute_merges(
     vocabulary: dict[int, bytes],
     vocab_size: int,
     pretokenized_dict: dict[tuple[bytes], int],
-    paired_tokens: tuple[bytes, bytes],
+    paired_tokens: dict[tuple[bytes, bytes], int],
 ) -> list[tuple[bytes, bytes]]:
     start_time = time.time()
 
     merges = []
 
     for i in range(vocab_size - len(vocabulary)):
-        """ if len(paired_tokens) == 0:
+        if len(paired_tokens) == 0:
             return (vocabulary, merges)
-         """
+        
+        print(f"\rMerge iteration: {i}", end="", flush=True)
+        
         most_frequent_pair = find_most_frequent_pair(paired_tokens)
         token_id = len(vocabulary)
         vocabulary[token_id] = most_frequent_pair[0] + most_frequent_pair[1]
@@ -149,6 +152,13 @@ def compute_merges(
     print(f"Time to build BPE merges: {end_time - start_time} seconds")
 
     return merges
+
+def find_most_frequent_pair(
+    paired_tokens: dict[tuple[bytes, bytes], int],    
+) -> tuple[bytes, bytes]:
+    max_entry_count = max(paired_tokens.values()) # max count of any pair
+
+    return max([k for k, v in paired_tokens.items() if v == max_entry_count]) # lexicographically greater pair if multiple max
 
 def merge_tokens(
     merged_tokens: dict[tuple[bytes], int],
