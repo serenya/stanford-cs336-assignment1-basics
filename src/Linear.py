153674011f7402503bf483706cc5cabf1a51620f
Sergii Rykov <serenya@live.com>
2025-10-19 14:15:46 +0200

    Implement linear module for Transformer

---
+++ src/Linear.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+class Linear(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None
+    ):
+        super(Linear, self).__init__()
+        self.weight = nn.Parameter(torch.empty(out_features, in_features)) #
+    
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        # Apply the linear transformation
+        return x @ self.weight.T.t()
+
+# Example usage
+if __name__ == "__main__":
+    # Create an instance of the Linear class
+    model = Linear(input_dim=3, output_dim=2)
+    print(model)
+
+    # Test with a random input tensor
+    input_tensor = torch.randn(1, 3)  # Batch size of 1, input_dim of 3
+    output = model(input_tensor)
+    print("Input:", input_tensor)
+    print("Output:", output)
